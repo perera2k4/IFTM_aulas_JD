03b57ec5cbaf65aca7d33a00574c9aa44126b0b7
perera2k4 <dev.bruno.carvalho@gmail.com>
2026-03-02 22:11:29 -0300

    Aula 03: Animação de status base e movimentação, adição de plataformas

---
+++ 2026-03-02/scenes/player.gd
@@ -0,0 +1,53 @@
+extends CharacterBody2D
+
+# Variáveis globais
+var speed = 200
+var gravity = 980
+var jumpVelocity = -400
+var attempJump = 0
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	pass
+	
+func _process(delta: float) -> void:
+	# Verificar se não está no chão e aplicar efeito da gravidade
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Executar movimentos laterais eixo X
+	var direction = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed
+	
+	# Verificar se o personagem está no chão
+	if is_on_floor():
+		attempJump = 0
+	
+	# Pulo e pulo duplo
+	if Input.is_action_just_pressed("jump"):
+		attempJump += 1
+		if attempJump <= 2:
+			velocity.y = jumpVelocity
+		else:
+			print("Sem pulos restantes")
+
+	move_and_slide()
+	
+	# Controle de animações
+	if is_on_floor():
+		if velocity.x == 0:
+			anim.play("idle")
+		else:
+			anim.play("walk")
+	else:
+		if Input.is_action_pressed("jump"):
+			anim.play("jump")
+		elif velocity.y > 0:
+			anim.play("fall")
+	
+	if direction < 0:
+		anim.flip_h = true
+	elif direction > 0:
+		anim.flip_h = false
+	
